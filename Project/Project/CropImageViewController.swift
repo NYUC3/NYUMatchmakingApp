b4d64a6929b7abecf2ecd841ec44ef84e2c9f23a
Vidyadhar Thatte <vd_thatte@yahoo.com>
2016-12-05 12:36:56 -0500

    commit after archive upload

--- Project/Project/CropImageViewController.swift
+++ Project/Project/CropImageViewController.swift
@@ -10,22 +10,6 @@ import UIKit
 
 class CropImageViewController: UIViewController {
 
-    var cropView: AKImageCropperView!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        let image = UIImage(named: "place.png")
-        //let frame = CGRectMake(0, 0, 300, 300)
-        let frame = CGRect(x: 0, y: 0, width: 300, height: 300)
-        cropView = AKImageCropperView(frame: frame, image: image!, showOverlayView: false)
-        
-        view.addSubview(cropView)
-    }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        cropView.refresh()
-    }
+ 
 
 }
